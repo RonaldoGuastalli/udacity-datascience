71ac247633c78dac933f48c9d28c37eca11b8a34
Ronaldo Guastalli <guastalli31@gmail.com>
2018-08-19 00:26:08 -0300

    Adicionado quiz: string e aspas

---
+++ fundamento-dsI/quiz/04-corrigir-citacao-string.py
@@ -0,0 +1,8 @@
+# TODO: Arrume essa String!
+#com aspas duplas:
+ford_quote = "Whether you think you can, or you think you can't--you're right."
+print('usando aspas duplas - '+ford_quote)
+
+#usando barra:
+ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
+print('usando barra - ' + ford_quote)

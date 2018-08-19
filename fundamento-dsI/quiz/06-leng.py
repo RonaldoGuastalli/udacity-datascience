16ce0d3bf76966ec381f4e53e3731a2a72f8215e
Ronaldo Guastalli <guastalli31@gmail.com>
2018-08-19 00:35:43 -0300

    Adicionado quiz: len()

---
+++ fundamento-dsI/quiz/06-leng.py
@@ -0,0 +1,14 @@
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+
+#nome completo
+nome_completo = given_name+ ' ' + middle_names+ ' ' + family_name
+print(nome_completo)
+
+#todo: calcular o tamanho desse nome
+name_length = len(nome_completo)
+
+# Confirme se o nome tem o número de caracteres permitido na Carteira de Motorista
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)

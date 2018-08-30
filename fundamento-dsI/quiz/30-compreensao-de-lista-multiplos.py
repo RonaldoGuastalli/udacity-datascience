53c1b1788a589ebc1a342f8b05a83dedb8ec00c1
Ronaldo Guastalli <guastalli31@gmail.com>
2018-08-30 15:07:37 -0300

    Adicionado quiz: compreensão de listas multiplos com range.

---
+++ fundamento-dsI/quiz/30-compreensao-de-lista-multiplos.py
@@ -0,0 +1,7 @@
+'''
+Quiz: Múltiplos de três
+Use uma compreensão de listas para criar uma lista multiples_3, contendo os 20 primeiros múltiplos de 3.
+'''
+
+multiples_3 = [3*x for x in range(1, 21)]
+print(multiples_3)

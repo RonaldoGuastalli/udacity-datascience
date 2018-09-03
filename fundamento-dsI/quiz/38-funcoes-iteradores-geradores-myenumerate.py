997f663251c6f0fae2c9cc590e1553c5a025af87
Ronaldo Guastalli <guastalli31@gmail.com>
2018-09-03 15:13:55 -0300

    adicionado Quiz: iteradores e geradores implementando função my_unumerate

---
+++ fundamento-dsI/quiz/38-funcoes-iteradores-geradores-myenumerate.py
@@ -0,0 +1,17 @@
+'''
+Quiz: Implementando my_enumerate
+Escreva sua própria função de gerador que funciona como a função interna enumerate.
+'''
+
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    """Implement your generator function here"""
+    count = start
+    for element in iterable:
+        yield count, element
+        count += 1
+
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))

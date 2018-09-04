51030f394168be66da5f4e27a635f23d8650ac11
Ronaldo Guastalli <guastalli31@gmail.com>
2018-09-04 11:36:42 -0300

    primeiro script.

---
+++ fundamento-dsI/quiz/40-scripting-editado.py
@@ -0,0 +1,22 @@
+'''
+Escrevendo seu próprio script
+Abra um novo arquivo vazio em seu editor de texto, nomeie e salve-o no local onde você mantém os arquivos de seu
+aprendizado de Python. Coloque o seguinte código no arquivo.
+'''
+
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
